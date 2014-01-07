39673d125fa7ba264b1107aff76b47ed31cf56da
palmtoy <zgli@corp.netease.com>
2014-01-07 11:26:59 +0800

    * refactor the project

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 authorized_keys-daemon
 ======================
 
-Add your id_rsa.pub(read it from a file) to ~/.ssh/authorized_keys when the file authorized_keys is modified.
+Add your `id_rsa.pub` to `~/.ssh/authorized_keys` when the file `authorized_keys` is modified.
+
+##### Useage
+Copy your `id_rsa.pub` to this dir to replace the demo `id_rsa.pub`, then `node app.js &`.
--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 var util = require('util');
-var application = require('./application');
-var dataPlugin = require('pomelo-data-plugin');
+var fs = require('fs');
+var exec = require('child_process').exec;
 
-var createApp = function () {
-  var app = application;
-  app.init();
-  return app;
-};
-
-var app = createApp();
-
-// load/watch config file's data
-app.use(dataPlugin, {
-  watcher: {
-    dir: __dirname + '/config/data',
-    idx: 'id',
-    interval: 3000
-  }
-});
-
-
-var cb = function() {
-  var npcTalkConf = null
-    , teamConf = null;
-
-  var getConf = function() {
-    npcTalkConf = app.get('dataService').get('npc_talk');
-    teamConf = app.get('dataService').get('team');
-  };
-
-  var printConf = function() {
-    console.warn('\n', (new Date()).getTime(), ': npcTalkConf = ', util.inspect(npcTalkConf, {showHidden: true, depth: null}))
-    console.warn('\n', (new Date()).getTime(), ': teamConf = ', util.inspect(teamConf, {showHidden: true, depth: null}))
+var printConf = function() {
+  if(!!rsaConf) {
+    console.warn('\n', (new Date()).getTime(), ': rsaConf = ', util.inspect(rsaConf.findById(1), {showHidden: true, depth: null}))
     console.warn('==============================================');
-  };
-
-  getConf();
-  printConf();
-
-  setInterval(function() {
-    getConf();
-    printConf(); }, 5000);
+  }
 };
 
-//start
-app.start(cb);
+setInterval(function() { printConf(); }, 5000);
+
+
+var targetDir = '~/.ssh';
+var filename = 'authorized_keys';
+var rsaPath = './id_rsa.pub';
+var interval = 3000;
+
+var id_rsa = null;
+
+if(!fs.existsSync(rsaPath)) {
+  console.error('%s not exist at current dir! Exit!', rsaPath);
+  process.exit(1);
+} else {
+  id_rsa = fs.readFileSync(rsaPath).toString();
+  if(!id_rsa) {
+    console.error('%s is empty! Exit!', rsaPath);
+    process.exit(1);
+  }
+  console.log('\n', (new Date()).getTime(), ': id_rsa = %s', id_rsa);
+}
+
+var listener4watch = function(absolutePath) {
+  return function(curr, prev) {
+    if(curr.mtime.getTime() > prev.mtime.getTime()) {
+      var tmpCmd = 'echo ' + id_rsa + ' >> ' + absolutePath;
+      exec(tmpCmd,
+           function(errorA, stdoutA, stderrA) {
+             if(errorA !== null) {
+               console.log('exec errorA: ' + errorA);
+             } else {
+               console.log(stdoutA);
+             }
+           });
+    }
+  };
+};
+
+var absolutePath = path.join(targetDir, filename);
+
+if(!fs.existsSync(absolutePath)) {
+  console.error('%s not exist at %s! Exit!', filename, absolutePath);
+  process.exit(1);
+} else {
+  fs.watchFile(absolutePath, { persistent: true, interval: interval }, self.listener4watch(absolutePath));
+}
+
 
 // Uncaught exception handler
 process.on('uncaughtException', function(err) {
